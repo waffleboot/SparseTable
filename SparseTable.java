6ca4c542ceb51429c3929dd223baa0b0088ade3e
Андрей <yangand@MacBook-Pro-Andrej.local>
2016-05-08 08:45:23 +0300

    change variable names

--- SparseTable.java
+++ SparseTable.java
@@ -20,20 +20,18 @@ public class Solution {
                 sptable[k][i] = Math.max(sptable[k-1][i],sptable[k-1][i + size]);
             }
         }
-//        for (int k = 0; k < depth; k++) {
-//            System.out.println(Arrays.toString(sptable[k]));
-//        }
         for (int i = 0; i < m; i++) {
             int size = scan.nextInt();
             int d = 31 - Integer.numberOfLeadingZeros(size);
             int globalMin = Integer.MAX_VALUE;
-            for (int a = 0; a < n - size + 1; a++) {
-                int b = a + size - 1;
-                int x = a;
-                int y = b - (1<<d) + 1;
-                //System.out.println("a=" + a + " b=" + b + " d=" + d + " x=" + x + " y=" + y);
-                int localMax = x == y ? sptable[d][x] : Math.max(sptable[d][x],sptable[d][y]);
-                if (localMax < globalMin) globalMin = localMax;
+            for (int startPos = 0; startPos < n - size + 1; startPos++) {
+                int endPos = startPos + size - 1;
+                int x = startPos;
+                int y = endPos - (1<<d) + 1;
+                int localMax = (x == y) ? sptable[d][x] : Math.max(sptable[d][x],sptable[d][y]);
+                if (localMax < globalMin) {
+                    globalMin = localMax;
+                }
             }
             System.out.println(globalMin);
         }
